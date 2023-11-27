c04e62be567a20aa4f4cdbac25f7b5414b406cb8
GiGa001 <igor.pelivanovic@gmail.com>
2023-11-27 10:38:21 +0100

    update typeWritter directive

--- src/app/directives/horizontal-scroll.directive.ts
+++ src/app/directives/horizontal-scroll.directive.ts
@@ -30,6 +30,7 @@ export class HorizontalScrollDirective implements AfterViewInit {
   }
   private initSectionHeight():void{
     let minHeightSection : number = Number(getComputedStyle(this._el).getPropertyValue('min-height').split('p')[0])
+    console.log(this.borderContainer?.nativeElement.offsetWidth)
     let extensionHeightSection : number = (this.elementsContainer?.nativeElement.children.length - 1) * this.borderContainer?.nativeElement.offsetWidth
     this.render.setStyle(this._el, 'height', `${minHeightSection + extensionHeightSection}px`)
   }
